ce9083972a42faa15f21b1c3b286ca5d1e30563c
God believer <oldmoonlight11@gmail.com>
2025-03-29 13:11:37 +0300

    nothing
    nothing

---
+++ Assets/Actual Scripts/FirstPersonStuff/HeadUSAGE.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Marchenkostuff
+{
+
+    #region Variables
+
+
+
+
+
+    #endregion
+
+}
+
+
---
+++ Assets/Scriptable_Objects/Head_Usage_DATA.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Marchenkostuff
+{
+    [CreateAssetMenu(fileName = "HeadUsageData", menuName = "FistPersonController/Data/HeadBobData", order = 3 )]
+
+    public class HeadUsageData : ScriptableObject 
+    {
+        #region Variables
+
+        
+
+
+        #endregion
+
+    }
+
+
+
+
+
+
+}
+
+
