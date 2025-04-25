e968b1c9f5e6458908619dcaf0e9cea037b1b995
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-04-25 23:31:23 +0300

    i have no words

--- Assets/QuakeLikeCombat/CoreScripts/RayCast.cs
+++ Assets/QuakeLikeCombat/CoreScripts/RayCast.cs
@@ -1,34 +1,15 @@
-using UnityEditor.Analytics;
-using UnityEngine;
-using UnityEngine.Animations.Rigging;
-using UnityEngine.Rendering;
+using UnityEngine;  
 
-
- 
-public class ExampleClass : MonoBehaviour
-
+namespace Marchenko
 {
-    LayerMask layerMask = LayerMask.GetMask("Wall", "Character");
-
-    // See Order of Execution for Event Functions for information on FixedUpdate() and Update() related to physics queries
-    void FixedUpdate()
+    public class RayCast : MonoBehaviour
     {
-
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-
-        { 
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow); 
-            Debug.Log("Did Hit"); 
-        }
-        else
-        { 
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white); 
-            Debug.Log("Did not Hit"); 
-        }
-
+        LayerMask Q3Enemylayer = LayerMask.GetMask("Enemy"); 
+        //need to use vector3 later 
+        public bool RayCastHit; 
+     
     }
 
 }
- 
+
+
