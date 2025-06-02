acff7a06aae8a9b6edb201c2ab05506429d0b099
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-06-02 23:32:20 +0300

    reference for the start

--- Assets/RetroThirdPerson/Movement/CharacterMovement.cs
+++ Assets/RetroThirdPerson/Movement/CharacterMovement.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
 
-public class CharacterMovement
+namespace RetroThirdPerson
 {
+    #region Data/Reference
+    public class CharacterMovement : MonoBehaviour
+    {
+        [Header("Reference")]
+        public Transform trans;
+        public Transform modelTrans;
+        public CharacterMovement characterController;
+    }
+    #endregion
+
+    #region Movemet
+
     
 }
+
+
