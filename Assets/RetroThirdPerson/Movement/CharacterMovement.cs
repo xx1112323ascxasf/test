ac276723055d6285a4e05e2cd9816cee1b1c3e72
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-06-05 23:40:32 +0300

    working all day with bad idea
    using rigidbody as actual main movement method

--- Assets/RetroThirdPerson/Movement/CharacterMovement.cs
+++ Assets/RetroThirdPerson/Movement/CharacterMovement.cs
@@ -2,19 +2,10 @@ using UnityEngine;
 
 namespace RetroThirdPerson
 {
-    #region  Stuff
+
     public class CharacterMovement : MonoBehaviour
     {
-        [Header("Reference")]
-        public Transform trans;
-        public Transform modelTrans;
-        public CharacterMovement characterController;
+        
+
     }
-
-    #endregion
-
-
-    
 }
-
-
