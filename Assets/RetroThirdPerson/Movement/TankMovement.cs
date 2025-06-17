1f17b09773368d448c5c4e827789b4a190728ed2
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-06-17 21:16:21 +0300

    bad code

--- Assets/RetroThirdPerson/Movement/TankMovement.cs
+++ Assets/RetroThirdPerson/Movement/TankMovement.cs
@@ -5,150 +5,19 @@ using UnityEngine.Scripting.APIUpdating;
 public class TankMovement : MonoBehaviour
 
 {
-    public bool tankControls = true;
-
-    public float speed;
-
-    float currentSpeed;
-    float gravity = 20f;
-
-    float walkSpeed = 1.5f;
-    float runSpeed = 4f;
-    float backSpeed = 1f;
-    float turnSpeed = 150f;
-
-
-    float horizontalInput;
-    float verticalInput;
-
-    //States
-    public bool isWaling;
-    bool runPressed;
-    public bool isRunning;
-
-    //Reference
-    Player player;
-    CharacterController characterController;
-    GameObject gameCamera;
-
-
-
-    void Start()
-    {
-        player = GetComponent<Player>();
-        characterController = GetComponent<CharacterController>();
-        //может вызывать ошибки
-        GameObject gameCamera = GameObject.FindGameObjectsWithTag("MainCamera")[0];
-    }
+    public float moveSpeed = 5f;
 
     void Update()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-        runPressed = Input.GetKey(KeyCode.LeftShift);
-
-        if (!player.isAaiming)
+        // Check for forward input (W or UpArrow)
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            if (horizontalInput > 0)
-            {
-                horizontalInput = 1;
-            }
-            else if (horizontalInput < 0)
-            {
-                horizontalInput = -1;
-            }
-            else
-            {
-                horizontalInput = 0;
-            }
-            if (verticalInput > 0)
-            {
-                verticalInput = 1;
-            }
-            else if (verticalInput < 0)
-            {
-                verticalInput = -1;
-            }
-            else
-            {
-                verticalInput = 0;
-            }
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
 
-
-
-
-        if (!player.stopInput)
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            if (tankControls)
-            {
-                float he = horizontalInput * Time.deltaTime * turnSpeed;
-                float ve = verticalInput * Time.deltaTime * speed;
-
-                Move(he, ve);
-            }
-        }
-        else
-        {
-            float he = horizontalInput * Time.deltaTime * speed;
-            float ve = verticalInput * Time.deltaTime * speed;
-
-            Move(he, ve);
-        }
-
-
-        bool movingForward = false;
-        bool movingBackward = false;
-        if (verticalInput > 0)
-        {
-            if (movingForward = false)
-            {
-                movingForward = true;
-            }
-        }
-        else if (verticalInput < 0)
-        {
-            if (tankControls)
-            {
-                movingForward = false;
-                movingBackward = true;
-            }
-            {
-                movingForward = true;
-            }
-
-        }
-        else if (!tankControls && horizontalInput != 0)
-        {
-            movingForward = true;
-        }
-        else
-        {
-            movingForward = false;
-        }
-        if (movingForward && runPressed)
-        {
-            isRunning = true;
-            speed = backSpeed;
-        }
-        else
-        {
-            isRunning = false;
-            speed = walkSpeed;
-        }
-    }
-
-    void Move(float he, float ye)
-    {
-        if (tankControls)
-        {
-            Vector3 move = new Vector3(0, 0, verticalInput);
-            move = transform.TransformDirection(move);
-            move.y -= gravity * Time.deltaTime;
-            characterController.Move(move);
-
-            Vector3 turn = new Vector3(0, he, 0);
-            transform.Rotate(turn);
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
         }
     }
 }
