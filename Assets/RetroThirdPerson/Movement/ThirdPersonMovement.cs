5182ff22700969427ede982681e1260a1e63718c
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-07-04 23:57:39 +0300

    camera orientated stuff

--- Assets/RetroThirdPerson/Movement/ThirdPersonMovement.cs
+++ Assets/RetroThirdPerson/Movement/ThirdPersonMovement.cs
@@ -1,31 +1,42 @@
-using System.IO;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-using UnityEngine.Scripting.APIUpdating;
 
 namespace RetroThirdPerson
 {
-
+    [RequireComponent(typeof(Rigidbody))]
     public class ThirdPersonMovement : MonoBehaviour
     {
+        Transform cam;
+        public float speed = 2;
         public float forwardSpeed = 6f;
         public float sidewaysSpeed = 4f;
-        //заставить это работать
-        public Transform cameraTransform;
-        private Vector3 moveHorizontal = Vector3.zero;
+
         private Rigidbody rb;
 
         void Start()
         {
+            cam = Camera.main.transform;
             rb = GetComponent<Rigidbody>();
         }
 
         void Update()
         {
+            Vector3 forwardMovement = cam.forward * Input.GetAxis("Vertical");
+            Vector3 horizontalMovement = cam.right * Input.GetAxis("Horizontal");
 
+            Vector3 movement = Vector3.ClampMagnitude(forwardMovement + horizontalMovement, 1);
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
+            //movement
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            float x = h * sidewaysSpeed;
+            float z = v * forwardSpeed;
+
+            Vector3 movement = new Vector3(x, rb.linearVelocity.y, z); // сохраняем вертикальную скорость
+
+            rb.linearVelocity = movement;
         }
     }
+}
 
-}   
-
-
+  
