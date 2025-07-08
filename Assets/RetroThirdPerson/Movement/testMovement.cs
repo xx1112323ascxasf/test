1dd73a26a73f6ae955942a7475dd5eecf99248da
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-07-08 22:51:24 +0300

    another way for simple things

--- Assets/RetroThirdPerson/Movement/testMovement.cs
+++ Assets/RetroThirdPerson/Movement/testMovement.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
 
-public class testMovement
+public class testMovement : MonoBehaviour
 {
-    
-}
+
+    float speed = 20f;
+    private float rbMovement;
+    public float rbMOvementSpeed;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>(); 
+        rbMovement = gameObject.transform.position.x
+    }
+    void FixedUpdate()
+    {
+        
+
+           
+        
+    }
+}   
