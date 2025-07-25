7b6b7362563bf061d16997abf6ac429c28b54e84
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-07-25 07:44:48 +0300

    testing rotation

--- Assets/RetroThirdPerson/Movement/ThirdPersonMovement.cs
+++ Assets/RetroThirdPerson/Movement/ThirdPersonMovement.cs
@@ -7,22 +7,28 @@ namespace RetroThirdPerson
     {
 
 
-        private Rigidbody rb;
+        private Rigidbody m_Rigitbody;
+        Vector3 m_EulerAngleVelocity;
 
 
         void Start()
         {
-            rb = GetComponent<Rigidbody>();
-            if (rb == null)
+            m_Rigitbody = GetComponent<Rigidbody>();
+            if (m_Rigitbody == null)
             {
                 Debug.LogError("NoRigitBoddy");
             }
+
+            // actual movement
+            m_EulerAngleVelocity = new Vector3(0, 2, 1);
 
         }
 
         void FixedUpdate()
         {
-            
+            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime);
+            m_Rigitbody.MoveRotation(m_Rigitbody.rotation * deltaRotation);
+
         }
     }
-}
+}  
