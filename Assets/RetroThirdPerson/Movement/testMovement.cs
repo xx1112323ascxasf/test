a3daff05f8f6e990793a3c1aacd139caaff6e414
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-07-31 22:46:07 +0300

    back to charactercontroller

--- Assets/RetroThirdPerson/Movement/testMovement.cs
+++ Assets/RetroThirdPerson/Movement/testMovement.cs
@@ -1,23 +1,38 @@
 using UnityEngine;
 
-public class testMovement : MonoBehaviour
+public class PlayerMovementNoJump : MonoBehaviour
 {
+    public CharacterController controller;
+    public float moveSpeed = 5f;
+    public float gravity = 9.81f; 
 
-    float speed = 20f;
-    private float rbMovement;
-    public float rbMOvementSpeed;
-    private Rigidbody rb;
+    private Vector3 velocity;
 
     void Start()
     {
-        //rb = GetComponent<Rigidbody>(); 
-        rbMovement = gameObject.transform.position.x;
+        controller = GetComponent<CharacterController>();
     }
-    void FixedUpdate()
-    {
-        
 
-           
-        
+    void Update()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.right * horizontalInput + transform.forward * verticalInput;
+        moveDirection.Normalize(); 
+
+        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1f; 
+        }
+        else
+        {
+            velocity.y -= gravity * Time.deltaTime;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
     }
-}   
+}
