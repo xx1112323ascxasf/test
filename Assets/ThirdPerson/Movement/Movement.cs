4d3574274096418f6e2ac65dbf23cd7f360a6ef0
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-08-13 23:27:00 +0300

    cursorlock

--- Assets/ThirdPerson/Movement/Movement.cs
+++ Assets/ThirdPerson/Movement/Movement.cs
@@ -1,71 +1,25 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 
-namespace ThirdPerson
+public class PlayerCamera : MonoBehaviour
 {
+    public float sensivityX;
+    public float sensivityY;
 
+    public Transform orientation;
 
-    public class Movement : MonoBehaviour
+    float xRotation;
+    float yRotation;
+
+    private void Start()
     {
-        [Header("Moverment")]
-        public float moveSpeed;
-        float horizontalInput;
-        float verctialInput;
-        Vector3 moveDirection;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 
-        
-
-
-
-        [Header("Reference")]
-        public Transform orientation;
-        public Transform player;
-        public Transform playerObj;
-        public Rigidbody rb;
-
-        public float rotationSpeed;
-
-        void Start()
-        {
-            rb = GetComponent<Rigidbody>();
-
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;    
-        }
-
-        private void MyInput()
-        {
-            horizontalInput = Input.GetAxisRaw("Horizontal");
-            verctialInput = Input.GetAxisRaw("Vertical");
-        }
-        
-        private void MovePlayer()
-        {
-            moveDirection = orientation.forward * verctialInput + orientation.right * horizontalInput;
-
-            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-        }
-
-        void FixedUpdate()
-        {
-            MovePlayer();  
-        }
-        void Update()
-        {
-            MyInput();
-
-
-                //rotate orientation
-            Vector3 viewDir = player.position - new Vector3(transform.position.x, transform.position.y, transform.position.z);
-            orientation.forward = viewDir.normalized;
-
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verctialInput = Input.GetAxis("Vertical");
-            Vector3 inputDir = orientation.forward * verctialInput + orientation.right * horizontalInput;
-
-            if (inputDir != Vector3.zero)
-                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
-        }
     }
 
+    private void Update()
+    {
+        
+    }
 }
