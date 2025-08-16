c10b53b9ed56e92664ac09b94a0c1821ca73d63f
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-08-16 23:15:12 +0300

    full working movement with orientation script

--- Assets/ThirdPerson/Camera/PlayerCamera.cs
+++ Assets/ThirdPerson/Camera/PlayerCamera.cs
@@ -27,10 +27,10 @@ public class PlayerCamera : MonoBehaviour
         yRotation += mouseX;
         xRotation -= mouseY;
 
-        xRotation = Mathf.Clamp(xRotation, 90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
         
     }
 }
--- Assets/ThirdPerson/Movement/PlayerMovement.cs
+++ Assets/ThirdPerson/Movement/PlayerMovement.cs
@@ -3,7 +3,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-    private float moveSpeed;
+    public float moveSpeed;
+    public float groundDrag;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+
     public Transform orientation;
 
     float horizontalInput;
@@ -22,22 +30,46 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         MyInput();
+        GroundCheck();
+        SpeedControl();
     }
     void FixedUpdate()
     {
-        MovePlayer();   
+        MovePlayer();
     }
 
     private void MyInput()
     {
-        horizontalInput = Input.GetAxisRaw("Horizonta;");
-        verctialInput = Input.GetAxisRaw("Verctial");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verctialInput = Input.GetAxisRaw("Vertical");
 
     }
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verctialInput + orientation.right * horizontalInput;
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+    private void GroundCheck()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        if (grounded)
+            rb.linearDamping = groundDrag;
+        else
+            rb.linearDamping = 0;
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        if (flatVelocity.magnitude > moveSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVelocity.x, rb.linearVelocity.y, limitedVelocity.z);
+
+        }
+
     }
 
 }
