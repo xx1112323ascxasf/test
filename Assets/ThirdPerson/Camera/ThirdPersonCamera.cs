782f234a51c09c334b9eb3467b145e9e6d65753a
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-08-20 17:54:09 +0300

    camera script

--- Assets/ThirdPerson/Camera/ThirdPersonCamera.cs
+++ Assets/ThirdPerson/Camera/ThirdPersonCamera.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
+
+    private void Sart()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false; 
+    }
     public Transform orientation;
     public Transform player;
     public Transform PlayerObject;
@@ -13,5 +19,13 @@ public class ThirdPersonCamera : MonoBehaviour
     {
         Vector3 viewDirection = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDirection.normalized;
+
+        float horizontalInput = Input.GetAxisRaw("Horizonta;");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 inputDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if (inputDirection != Vector3.zero)
+            PlayerObject.forward = Vector3.Slerp(PlayerObject.forward, inputDirection.normalized, Time.deltaTime * rotationSpeed);
+
     }
 }
