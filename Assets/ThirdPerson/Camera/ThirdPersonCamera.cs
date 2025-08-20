8bd4241a6c2ca688b42adcbb6f7766a43f341c88
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-08-20 23:00:11 +0300

    huge scene setup
    game actually start looking like a game

--- Assets/ThirdPerson/Camera/ThirdPersonCamera.cs
+++ Assets/ThirdPerson/Camera/ThirdPersonCamera.cs
@@ -3,11 +3,7 @@ using UnityEngine;
 public class ThirdPersonCamera : MonoBehaviour
 {
 
-    private void Sart()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false; 
-    }
+
     public Transform orientation;
     public Transform player;
     public Transform PlayerObject;
@@ -15,17 +11,43 @@ public class ThirdPersonCamera : MonoBehaviour
 
     public float rotationSpeed;
 
+    public Transform combatLookAt;
+
+    public CameraStyle currentStyle;
+
+    public enum CameraStyle
+    {
+        Basic,
+        Combat
+    }
+    
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
     public void Update()
     {
         Vector3 viewDirection = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDirection.normalized;
 
-        float horizontalInput = Input.GetAxisRaw("Horizonta;");
-        float verticalInput = Input.GetAxisRaw("Vertical");
-        Vector3 inputDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        if (currentStyle == CameraStyle.Basic)
+        {
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
+            Vector3 inputDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        if (inputDirection != Vector3.zero)
-            PlayerObject.forward = Vector3.Slerp(PlayerObject.forward, inputDirection.normalized, Time.deltaTime * rotationSpeed);
+            if (inputDirection != Vector3.zero)
+                PlayerObject.forward = Vector3.Slerp(PlayerObject.forward, inputDirection.normalized, Time.deltaTime * rotationSpeed);
+        }
+        else if (currentStyle == CameraStyle.Combat)
+        {
+            Vector3 directoryToCombat = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+            orientation.forward = directoryToCombat.normalized;
+
+            PlayerObject.forward = directoryToCombat.normalized;
+        }
 
     }
 }
