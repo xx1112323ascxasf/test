2d94f465c2ec0db1da5844873a45cb1c591c319a
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-08-29 22:58:56 +0300

    variables

--- Assets/FirstPerson/Movement/FirstPersonMovement.cs
+++ Assets/FirstPerson/Movement/FirstPersonMovement.cs
@@ -4,6 +4,8 @@ namespace FirstPerson
 {
     public class FirstPersonMovement : MonoBehaviour
     {
+
+        
         public class MovementSettings
         {
             public float MaxSpeed;
@@ -19,6 +21,18 @@ namespace FirstPerson
         }
 
 
+        //movement
+        [SerializeField] private float m_Friction = 6;
+        [SerializeField] private float m_Gravity = 20;
+        [SerializeField] private float m_Jumpforce = 8;
+        //air control
+        [SerializeField] float m_AirControl = 0.3f;
+        [SerializeField] private MovementSettings m_GroundSettings = new MovementSettings(7, 14, 10);
+        [SerializeField] private MovementSettings m_AirSettings = new MovementSettings(7, 2, 2);
+        [SerializeField] private MovementSettings m_StrafeSettings = new MovementSettings(1, 50, 50);
+
+
+        //Return player current speed
 
 
         private void Start()
