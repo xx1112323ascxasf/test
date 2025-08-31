f280029dacad0414bcca690a64d9fbfd9d02a54c
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-08-31 21:38:12 +0300

    animations rigging fix

--- Assets/FirstPerson/Movement/FirstPersonMovement.cs
+++ Assets/FirstPerson/Movement/FirstPersonMovement.cs
@@ -6,44 +6,8 @@ namespace FirstPerson
     {
 
         
-        public class MovementSettings
-        {
-            public float MaxSpeed;
-            public float Accleration;
-            public float Decalarion;
 
-            public MovementSettings(float maxSpeed, float accel, float decel)
-            {
-                MaxSpeed = maxSpeed;
-                Accleration = accel;
-                Decalarion = decel;
-            }
-        }
-
-
-        //movement
-        [SerializeField] private float m_Friction = 6;
-        [SerializeField] private float m_Gravity = 20;
-        [SerializeField] private float m_Jumpforce = 8;
-        //air control
-        [SerializeField] float m_AirControl = 0.3f;
-        [SerializeField] private MovementSettings m_GroundSettings = new MovementSettings(7, 14, 10);
-        [SerializeField] private MovementSettings m_AirSettings = new MovementSettings(7, 2, 2);
-        [SerializeField] private MovementSettings m_StrafeSettings = new MovementSettings(1, 50, 50);
-
-
-        //Return player current speed
-
-
-        private void Start()
-        {
-
-        }
-
-        private void Update()
-        {
-
-        }
+        
         
 
         
