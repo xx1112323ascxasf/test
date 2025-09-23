f7e592bb02c5905e6c01f9bfebd5207d759a9c7d
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-09-23 22:55:39 +0300

    newmovementsystemtest

---
+++ Assets/UnityNewMovement/FirstPersonController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+namespace FirstPersonMovement
+{
+    public class FirstPersonController : MonoBehaviour
+    {
+        [Header("Variables")]
+        private GameInput _gameInput;
+        private Rigidbody rg;
+        public float moveSpeed = 10f; 
+         
+        
+
+
+
+    }
+}
+
