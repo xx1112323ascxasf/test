1e49befa5392b159acd13a3bbdbc8ea488daa6a0
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-09-29 13:24:25 +0300

    wasd input

--- Assets/Gameplay/FirstPerson/Scripts/PlayerInput.cs
+++ Assets/Gameplay/FirstPerson/Scripts/PlayerInput.cs
@@ -1,6 +1,20 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerInput
+
+public class PlayerInput : MonoBehaviour
 {
-    
+    private PlayerInput playerControlls;
+    private Vector2 _moveAxis;
+
+    void Awake()
+    {
+        playerControlls = new PlayerInput();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
 }
