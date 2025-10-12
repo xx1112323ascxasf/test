092ec2d86ab4c63de4a6df6c5c16417d28d1d9d2
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-10-12 23:26:31 +0300

    invoke method

--- Assets/InputSystem/Scripts/Movement.cs
+++ Assets/InputSystem/Scripts/Movement.cs
@@ -1,6 +1,20 @@
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
-public class Movement
-{
-    
-}
+
+public class Movement : MonoBehaviour
+{ 
+    //add to inspector 
+     public InputActionReference fire;
+    public InputActionReference move;
+
+    public SpeedTreeWindAsset force; 
+
+    public void Dash(InputAction.CallbackContext context)
+    {
+        // B KEY 
+        Debug.Log("SussyBaka");
+    }
+}  
