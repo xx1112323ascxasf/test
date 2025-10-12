218876b42da75455391beef7a2516c64d441dfef
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-10-13 00:04:19 +0300

    input system backup

--- Assets/InputSystem/Scripts/Movement.cs
+++ Assets/InputSystem/Scripts/Movement.cs
@@ -5,16 +5,46 @@ using UnityEngine.InputSystem;
 
 
 public class Movement : MonoBehaviour
-{ 
+{
+    #region test 
     //add to inspector 
-     public InputActionReference fire;
-    public InputActionReference move;
+    public InputActionReference fire;
 
-    public SpeedTreeWindAsset force; 
+
+
+
 
     public void Dash(InputAction.CallbackContext context)
     {
         // B KEY 
         Debug.Log("SussyBaka");
+    }
+
+    #endregion
+
+
+    private Vector2 moveInput;
+    public float moveSpeed = 10.0f;
+    public Rigidbody frb;
+    public Vector3 fmovement;
+
+
+    public void Start()
+    {
+        frb = GetComponent<Rigidbody>();
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        Debug.Log("Move");
+    }
+
+    public void FixedUpdate()
+    {
+        
+
+        frb.linearVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+    
     }
 }  
