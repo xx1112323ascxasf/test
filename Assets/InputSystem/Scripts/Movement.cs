e32887af429f0368264b2ef08e3374729fae0f44
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-10-19 00:47:18 +0300

    fixed camera and movement

--- Assets/InputSystem/Scripts/Movement.cs
+++ Assets/InputSystem/Scripts/Movement.cs
@@ -6,45 +6,41 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    #region test 
-    //add to inspector 
+    #region Test
     public InputActionReference fire;
-
-
-
-
 
     public void Dash(InputAction.CallbackContext context)
     {
         // B KEY 
         Debug.Log("SussyBaka");
     }
-
     #endregion
-
 
     private Vector2 moveInput;
     public float moveSpeed = 10.0f;
-    public Rigidbody frb;
-    public Vector3 fmovement;
+    private Rigidbody frb;
 
-
-    public void Start()
+    private void Start()
     {
         frb = GetComponent<Rigidbody>();
     }
+
     public void Move(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-
         Debug.Log("Move");
     }
 
-    public void FixedUpdate()
+    private void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y);
 
-        frb.linearVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
-    
+        frb.AddForce(movement * moveSpeed, ForceMode.Force);
+
+        if (movement.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            frb.MoveRotation(Quaternion.Slerp(frb.rotation, targetRotation, Time.fixedDeltaTime * 10f));
+        }
     }
-}  
+}
