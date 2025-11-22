2b5fbd648303db0ad12ac7a8302ff736ea0491bc
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-11-22 23:40:26 +0300

    back to vectors

--- Assets/InputSystem/Scripts/CharacterRotation.cs
+++ Assets/InputSystem/Scripts/CharacterRotation.cs
@@ -3,32 +3,7 @@ using UnityEngine;
 public class CharacterRotation : MonoBehaviour
 {
  
-    public Rigidbody rb;
-    public Transform cameraTransform; 
 
-    public float rotationSpeed = 40f;
-
-    void Start()
-    {
-        Cursor.visible = false;                
-        Cursor.lockState = CursorLockMode.Locked; 
-    }
-
-    void FixedUpdate()
-    {
-        if (cameraTransform == null || rb == null) return;
-
-        Vector3 targetDirection = cameraTransform.forward;
-        targetDirection.y = 0f;
-
-        if (targetDirection.sqrMagnitude > 0.01f)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
-
-            Quaternion smoothedRotation = Quaternion.Slerp(rb.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
-
-            rb.MoveRotation(smoothedRotation);
-        }
-    }
+    
 
 }
--- Assets/InputSystem/Scripts/Movement.cs
+++ Assets/InputSystem/Scripts/Movement.cs
@@ -6,174 +6,25 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
 
-    
- 
-    public float forwardSpeed = 1f;
-    public float leftSpeed = 1.5f;
-    public float rightSpeed = 1.5f;
-    public float backwardSpeed = 1f;
+  public float moveForce = 10f;
 
     private Rigidbody rb;
-    private bool isMovingForward;
-    private bool isMovingLeft;
-    private bool isMovingRight;
-    private bool isMovingBackward;
-    void Start()
+    private Vector2 moveInput;
+
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
 
-    #region input actions
-
-
-    public void MoveForward(InputAction.CallbackContext context)
+    public void OnMove(InputAction.CallbackContext context)
     {
-        if (context.started)
-        {
-            isMovingForward = true;
-        }
-        else if (context.canceled)
-        {
-            isMovingForward = false;
-        }
-    }
-    public void MoveLeft(InputAction.CallbackContext context)
-    {
-        if (context.started)
-        {
-            isMovingLeft = true;
-        }
-        else if (context.canceled)
-        {
-            isMovingLeft = false;
-        }
-    }
-    public void MoveRight(InputAction.CallbackContext context)
-    {
-        if (context.started)
-        {
-            isMovingRight = true;
-        }
-        else if (context.canceled)
-        {
-            isMovingRight = false;
-        }
-    }
-    public void MoveBackward(InputAction.CallbackContext context)
-    {
-        if (context.started)
-        {
-            isMovingBackward = true;
-        }
-        else if (context.canceled)
-        {
-            isMovingBackward = false;
-        }
+        moveInput = context.ReadValue<Vector2>();
     }
 
-
-
-    public void isMoving()
+    private void FixedUpdate()
     {
-         if (isMovingForward)
-        {
-            rb.AddForce(transform.forward * forwardSpeed , ForceMode.Impulse); 
-        }
-        if (isMovingLeft)
-        {
-            rb.AddForce(-transform.right * leftSpeed , ForceMode.Impulse); 
-        }
-        if (isMovingRight)
-        {
-            rb.AddForce(transform.right * rightSpeed , ForceMode.Impulse);
-        }
-        if (isMovingBackward)
-        {
-            rb.AddForce(-transform.forward * backwardSpeed , ForceMode.Impulse);
-        }
+        Vector3 force = new Vector3(moveInput.x, 0f, moveInput.y) * moveForce;
+        rb.AddForce(force, ForceMode.Force);
     }
-
-
-
-
-
-    #endregion
-    //global/world directions (Vector3.forward, Vector3.right)
-    // thats why here is transform insted of Vector3.
-    // left = negative right
-
-    #region speedcontrol
-
-    [Header("Movement")]
-
-    public float JumpForce;
-    public float JumpCooldown;
-    public float airMultiplayer;
-
-    bool readyToJump; 
-
-
-
-    public float groundDrag;
-
-    [Header("Ground Check")]
-
-    public float playerHeight;
-
-    public LayerMask whatIsGround;
-
-   
-
-    bool grounded;
-    public void GroundCheck()  //later make spherecast
-    {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
-
-        if(grounded)
-        {
-            rb.linearDamping = groundDrag;
-        }
-        else
-        {
-            rb.linearDamping = 0;
-        }
-       
-    }
-
-    
-    public void Jump(InputAction.CallbackContext context)
-    {
-        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-        rb.AddForce(transform.up * JumpForce, ForceMode.Impulse);
-
-
-    }
-
-    public void ResetJump()
-    {
-        readyToJump = true;  
-    }
-
-    
- 
-    
-
-  
-    #endregion
-    void FixedUpdate()
-    {
-        isMoving();
-        
-    }
-
-    void Update()
-    {
-        GroundCheck();
-    }
-
-
-
-
-
 }
