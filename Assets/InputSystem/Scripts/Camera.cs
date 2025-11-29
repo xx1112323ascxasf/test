47be393d9bd71b7819925731b9c9c93085cf8163
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-11-29 17:59:13 +0300

    character rotation

---
+++ Assets/InputSystem/Scripts/Camera.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+
+
+public class PlayerLook : MonoBehaviour
+{
+    #region characterortatin
+    
+    
+    public Transform cameraObject;   
+    public float rotationSpeed = 10f;   
+
+   
+
+    void RotateToCameraDirection()
+    {
+       
+        Vector3 cameraForward = cameraObject.forward;
+        cameraForward.y = 0f;                       
+        if (cameraForward.sqrMagnitude < 0.01f) return;
+
+       
+        Quaternion targetRotation = Quaternion.LookRotation(cameraForward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+
+    #endregion
+    #region cursor lock
+
+    void CursorLock()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            LockCursor();
+        }
+    }
+
+        void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;  
+        Cursor.visible = false;                    
+    }
+
+    #endregion
+
+
+    void Start()
+    {
+        LockCursor();
+    }
+
+
+    void Update()
+    {
+        CursorLock();
+        RotateToCameraDirection();
+    }
+}
