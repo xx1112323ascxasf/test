31800092a6d77487e2b3ba3d642b0d15dfa66364
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-12-18 23:31:24 +0300

    start of state machine

--- Assets/StateMachine/CharacterStateMachine.cs
+++ Assets/StateMachine/CharacterStateMachine.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
 
-public class CharacterStateMachine
+
+namespace HierarchicalSM
 {
-    
+    public abstract class State
+    {
+        public readonly StateMachine Machine;
+        public readonly State Parent;
+        public State ActiveChild;
+
+        public State(StateMachine machine, State parent = null)
+        {
+            Machine = machine;
+            Parent = parent;
+        }
+
+        protected virtual State GetInitialState() => null;
+        protected virtual State GetTransition()=> null;
+
+        protected virtual void OnEnter(){}
+        protected virtual void OnExit(){}
+        protected virtual void OnUpdate(float deltaTime) {} 
+
+    }
+
+
+    public class StateMachine{}
+
+    public class TransitionSequencer{}
+
 }
+
+
