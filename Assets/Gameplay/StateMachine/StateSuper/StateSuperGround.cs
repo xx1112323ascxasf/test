280e682e10117ed17ee7ef85ee7dae85f2486f4e
xx1112323ascxasf <oldmoonlight11@gmail.com>
2025-12-27 23:25:06 +0300

    freshstart

--- Assets/Gameplay/StateMachine/StateSuper/StateSuperGround.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class StateSuperGround : StateManager
-{
-    public StateSuperGround(PlayerGeneral playerGENERAL, StateMachine stateMACHINE) : base(playerGENERAL, stateMACHINE)
-    {
-        
-    }
-}
