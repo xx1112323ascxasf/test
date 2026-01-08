16d80949037e340e49eaf57fa6a5f7e1518687d8
xx1112323ascxasf <oldmoonlight11@gmail.com>
2026-01-08 23:05:57 +0300

    adding dash

---
+++ Assets/Gameplay/Movement/DashController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class DashController : MonoBehaviour
+{
+    public void Dash(InputAction.CallbackContext context)
+    {
+        
+    }
+
+}
--- Assets/Gameplay/StateMachine/IdleState.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-public class idleState : PlayerState 
-{
-    public override List<PlayerAction> GetActionsData();
-
-}
