98f8cc24bc180b9f1c7bde68793021ef15c17e6e
xx1112323ascxasf <oldmoonlight11@gmail.com>
2026-01-11 17:40:32 +0300

    character speed fix

--- Assets/Gameplay/Movement/DashController.cs
+++ Assets/Gameplay/Movement/DashController.cs
@@ -5,13 +5,11 @@ using UnityEngine.InputSystem;
 public class DashController : MonoBehaviour
 {
     [Header("Dash Settings")]
-    public float dashForce = 20f;
-    public float dashCooldown = 1f;
 
+
+    public float m_Thrust = 20f; 
     private Rigidbody rb;
-    private float lastDashTime;
 
-    private Vector3 moveDirection;
 
     void Awake()
     {
@@ -20,24 +18,16 @@ public class DashController : MonoBehaviour
 
     void FixedUpdate()
     {
-        moveDirection = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+        
     }
 
     public void OnDash(InputAction.CallbackContext context)
-    {
-        if (!context.performed) return;
+    {   
+            if (context.phase == InputActionPhase.Performed)
+        {
+            Debug.Log("dash!");
 
-        if (Time.time < lastDashTime + dashCooldown) return;
-
-        Vector3 dashDir;
-
-        if (moveDirection.magnitude > 0.1f)
-            dashDir = moveDirection.normalized;
-        else
-            dashDir = transform.forward; // если стоим на месте
-
-        rb.AddForce(dashDir * dashForce, ForceMode.Impulse);
-
-        lastDashTime = Time.time;
-    }
+            rb.AddForce(transform.forward * m_Thrust);
+        }
+    }   
 }
