85e3ab63b267ced387f758d62f54dba00d5e9499
xx1112323ascxasf <oldmoonlight11@gmail.com>
2026-03-20 23:04:33 +0300

    some optimizations

--- Assets/aiMovement/AiLocomotion.cs
+++ Assets/aiMovement/AiLocomotion.cs
@@ -7,15 +7,31 @@ namespace AiMovement
     {
         public Transform plaerTransform;
         NavMeshAgent agent;
+        public float maxTime = 1.0f;
+        public float maxDistance = 1.0f;
+        Animator animator;
+        float timer = 0.0f;
 
         void Start()
         {
             agent = GetComponent<NavMeshAgent>();
+            animator = GetComponent<Animator>();
         }
 
         void Update()
         {
-            agent.destination = plaerTransform.position; // Set the destination of the NavMeshAgent to the player's position
+            timer -= Time.deltaTime;
+            if(timer < 0.0f)
+            {
+                float sqDistance = (plaerTransform.position - agent.destination).sqrMagnitude;
+                if (sqDistance > maxDistance * maxDistance)
+                {
+                    agent.destination = plaerTransform.position;
+                }
+                agent.destination = plaerTransform.position;
+            }
+             // Set the destination of the NavMeshAgent to the player's position
+            animator.SetFloat("Speed", agent.velocity.magnitude); 
         }
     }
 
