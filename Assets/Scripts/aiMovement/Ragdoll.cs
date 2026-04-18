5dadd4eb8b92aa5bdb741f13dfae9b604dfccfaa
xx1112323ascxasf <oldmoonlight11@gmail.com>
2026-04-18 23:36:32 +0300

    collider shaking fix

---
+++ Assets/Scripts/aiMovement/Ragdoll.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    Rigidbody[] rigitBodies;
+    Animator animator;
+
+    void Start()
+    {
+        rigitBodies = GetComponentsInChildren<Rigidbody>();
+        DeactivateRagdoll();
+        animator = GetComponent<Animator>();
+    }
+
+    public void DeactivateRagdoll() //while not moving, the hitbox having error shaking, if not kinematic
+    {
+        foreach(var rigitBody in rigitBodies)
+        {
+            rigitBody.isKinematic = true;
+        }
+        animator.enabled = true;
+    }
+
+    public void ActivateRagdoll()
+    {
+        foreach (var rigitBody in rigitBodies)
+        {
+            rigitBody.isKinematic = false;
+        }
+        animator.enabled = false;
+    }
+
+    //the whole script helping ragdoll colliders to be static 
+}
