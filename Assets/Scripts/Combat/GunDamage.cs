d582253ee66fd3cd2064aabc58d4f49d9a1b8438
xx1112323ascxasf <oldmoonlight11@gmail.com>
2026-04-28 00:42:37 +0300

    basic scripts for ai enemy

--- Assets/Scripts/Combat/GunDamage.cs
+++ Assets/Scripts/Combat/GunDamage.cs
@@ -1,20 +1,37 @@
 using UnityEngine;
 
-public class GunDamage : MonoBehaviour
+public class GunDamage : RaycastWeapon
 {
-    public float Damage;
-    public float BulletRange;
-    private Transform playerCamera;
+    public Camera fpsCamera;
+    public float range = 100f;
+    public LayerMask hitLayers = ~0;
 
-    private void Start()
+    void Update()
     {
-        playerCamera = Camera.main.transform;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
     }
 
-    public void Shoot()
+    void Shoot()
     {
-     
- 
-      
+        Camera cam = fpsCamera != null ? fpsCamera : Camera.main;
+        if (cam == null) return;
+
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit, range, hitLayers))
+        {
+            HitBox hitBox = hit.collider.GetComponent<HitBox>();
+            if (hitBox == null && hit.rigidbody != null)
+            {
+                hitBox = hit.rigidbody.GetComponentInChildren<HitBox>();
+            }
+
+            if (hitBox != null)
+            {
+                hitBox.OnRaycastHit(this, hit.normal * -1f);
+            }
+        }
     }
 }
--- Assets/Scripts/Combat/HitBox.cs
+++ Assets/Scripts/Combat/HitBox.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
 
-public class HitBox
+public class HitBox : MonoBehaviour
 {
-    
+    public Health health;
+    Rigidbody partRigidbody;
+
+    void Awake()
+    {
+        partRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void OnRaycastHit(RaycastWeapon weapon, Vector3 direction)
+    {
+        if (weapon == null || health == null) return;
+
+        health.TakeDamage(weapon.damage, direction);
+        if (partRigidbody != null)
+        {
+            partRigidbody.AddForce(direction * weapon.knockback, ForceMode.Impulse);
+        }
+    }
 }
