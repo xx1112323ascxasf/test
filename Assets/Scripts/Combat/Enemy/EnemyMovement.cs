86d5e35dbdf3548561e5870b96e01efb12edc36f
xx1112323ascxasf <192695086+xx1112323ascxasf@users.noreply.github.com>
2026-05-27 00:18:11 +0300

    script update for animation with enemy ai

--- Assets/Scripts/Combat/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Combat/Enemy/EnemyMovement.cs
@@ -5,18 +5,49 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     public Transform Target;
+
+
+
+    [SerializeField] private Animator Animator; 
+    [SerializeField] private NavMeshAgent Agent;
+    private AgentLinkMover LinkMover; 
+
+    private const string IsWalking = "IsWalking";
+    private const string Jump = "Jump";
+    private const string Landed = "Landed";
+
+
+    
     public float UpdateSpeed = 0.1f; // how frequently to update the path 
-    private NavMeshAgent Agent;
+   
 
     private void Awake()
     {
         Agent = GetComponent<NavMeshAgent>();
+        LinkMover = GetComponent<AgentLinkMover>();
+
+        LinkMover.OnLinkEnd += HandleLinkEnd;
+        LinkMover.OnLinkStart += HandleLinkStart;
+
+        
     }
 
     public void Start()
     {
         StartCoroutine(FollowTarget());
-
+        
+    }
+    public void Update()
+    {
+        Animator.SetBool(IsWalking, Agent.velocity.magnitude > 0.01f);
+    }
+    private void HandleLinkStart()
+    {
+        Animator.SetTrigger(Jump);
+    }
+    private void HandleLinkEnd()
+    {
+        Animator.SetTrigger(Landed);
     }
     private IEnumerator FollowTarget()
     {
