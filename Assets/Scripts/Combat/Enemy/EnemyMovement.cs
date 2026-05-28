86175c78850f717c80f3e94934f77005a8b5766d
xx1112323ascxasf <192695086+xx1112323ascxasf@users.noreply.github.com>
2026-05-28 23:52:36 +0300

    deleting mess

--- Assets/Scripts/Combat/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Combat/Enemy/EnemyMovement.cs
@@ -8,63 +8,19 @@ public class EnemyMovement : MonoBehaviour
     public float UpdateSpeed = 0.1f; // how frequently to update the path 
 
 
-    [SerializeField] private Animator Animator; 
     [SerializeField] private NavMeshAgent Agent;
-    private AgentLinkMover LinkMover; 
-
-    private bool hasIsWalkingParameter;
-
-    public const string IsWalking = "IsWalking";
-    private const string Jump = "Jump";
-    private const string Landed = "Landed";
-
 
 
     private void Awake()
     {
-        if (Animator == null) Animator = GetComponent<Animator>();
         Agent = GetComponent<NavMeshAgent>();
-        LinkMover = GetComponent<AgentLinkMover>();
-
-        if (LinkMover != null)
-        {
-            LinkMover.OnLinkEnd += HandleLinkEnd;
-            LinkMover.OnLinkStart += HandleLinkStart;
-        }
-
-        hasIsWalkingParameter = false;
-        if (Animator != null)
-        {
-            var parms = Animator.parameters;
-            for (int i = 0; i < parms.Length; i++)
-            {
-                if (parms[i].name == IsWalking)
-                {
-                    hasIsWalkingParameter = true;
-                    break;
-                }
-            }
-        }
     }
 
     public void Start()
     {
         StartCoroutine(FollowTarget());
-        
     }
-    public void Update()
-    {
-        if (Animator != null && hasIsWalkingParameter)
-            Animator.SetBool(IsWalking, Agent.velocity.magnitude > 0.01f);
-    }
-    private void HandleLinkStart()
-    {
-        Animator.SetTrigger(Jump);
-    }
-    private void HandleLinkEnd()
-    {
-        Animator.SetTrigger(Landed);
-    }
+
     private IEnumerator FollowTarget()
     {
 
